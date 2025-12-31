3c5377b6a5c86aa240092282eda5e18e2ebeef5e
sahil agarwal <sahilagarwal20052005@gmail.com>
2025-12-31 23:41:35 +0530

    inital submission

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(helmet());
 app.use(mongoSanitize());
 
 app.use(cors({
-  origin:['http://localhost:3000'],
+  origin:['http://localhost:5173'],
   methods:['GET','POST','PUT','DELETE'],
   credentials:true
 }));
